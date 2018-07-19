2604215264dc2b18125855fb9cf040d195a7bb28
Mike Limberg <mlimberg@W-DN-ML-MLIMBER.local>
2018-07-19 12:48:13 -0600

    Call your mom

---
+++ test4.js
@@ -0,0 +1,7 @@
+const callMom = () => {
+  console.log('dialing 433-5528')
+  console.log('...ring...')
+  console.log('...ring...')
+  console.log('...ring...')
+  console.log('Hello! Mom here!')
+}
